eea4cdd635c028f09fbfff57c69324cefb7d5386
Tasneem08 <tasneem08@ufl.edu>
2017-09-22 17:00:53 -0400

    Reading command line args.

---
+++ project2/lib/MainController.ex
@@ -0,0 +1,8 @@
+defmodule MainController do
+
+# entry point to the code. Read command line arguments and invoke the right things here.
+  # Entry point to the code. 
+  def main(args) do
+   IO.inspect(args)
+  end
+end
