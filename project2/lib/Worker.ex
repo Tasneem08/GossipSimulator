dc23107fdf82e6971e49e8845e239c3dc5323eb5
Mugdha <mathkarmugdha@gmail.com>
2017-09-22 18:23:18 -0400

    Basic worker structure
    first draft

--- project2/lib/Worker.ex
+++ project2/lib/Worker.ex
@@ -1,3 +1,20 @@
 defmodule Worker do
-    
+
+
+def topology(topo) do
+case topo do
+    :line -> IO.puts "Found line"
+    :full ->IO.puts "Found full"
+    :2D ->IO.puts "Found 2D"
+    :imp2D ->IO.puts "Found imp2D"
+end
+end
+
+def receive(algo) do
+case algo do
+    :gossip -> "Use Gossip algo"
+    :push-sum -> "Use Push sum algo"
+
+
+end
 end
