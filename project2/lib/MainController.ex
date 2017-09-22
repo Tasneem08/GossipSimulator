b4c7bbe7719ed19b9fa1eba747cb9a759ef7c2da
Tasneem08 <tasneem08@ufl.edu>
2017-09-22 19:31:23 -0400

    Supervisor -rough draft

--- project2/lib/MainController.ex
+++ project2/lib/MainController.ex
@@ -6,5 +6,22 @@ defmodule MainController do
    IO.inspect(args)
    [nNodes,topology,algorithm] = args
    numNodes=nNodes|>String.to_integer()
+   Gossip.Supervisor.start_link(nNodes,topology,algorithm)
   end
+end
+
+defmodule Gossip.Supervisor do
+    use Supervisor
+
+    def start_link(nNodes,topology,algorithm) do
+    Supervisor.start_link(__MODULE__, [nNodes,topology,algorithm])
+    end
+
+    def init([nNodes,topology,algorithm]) do
+        children = [
+            worker(Worker, [nNodes,topology,algorithm])
+        ]
+
+        IO.inspect Supervisor.start_child(children,strategy: :one_for_one)
+    end
 end
--- project2/lib/Worker.ex
+++ project2/lib/Worker.ex
@@ -1,11 +1,26 @@
 defmodule Worker do
+use Agent
 
+  def start_link(nodeId,topology,algorithm) do
+    IO.inspect Agent.start_link(fn -> createWorker(nodeId,topology,algorithm) end)
+  end
+
+
+def createWorker(nodeId,topology,algorithm) do
+s = nodeId
+w = 1
+[s,w,0]
+end
+
+def init([nNodes,topology,algorithm]) do
+IO.puts("Reached here..")
+end
 
 def topology(topo) do
 case topo do
     :line -> IO.puts "Found line"
     :full ->IO.puts "Found full"
-    :2D ->IO.puts "Found 2D"
+    :twoD ->IO.puts "Found 2D"
     :imp2D ->IO.puts "Found imp2D"
 end
 end
@@ -13,8 +28,9 @@ end
 def receive(algo) do
 case algo do
     :gossip -> "Use Gossip algo"
-    :push-sum -> "Use Push sum algo"
+    :pushsum -> "Use Push sum algo"
 
 
+end
 end
 end
